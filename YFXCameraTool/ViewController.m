f879483dbf88c80d85f899a814c896c86dcd6278
xue-ios <122092669@qq.com>
2017-05-23 10:45:35 +0800

    Initial Commit

---
+++ YFXCameraTool/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YFXCameraTool
+//
+//  Created by fangxue on 2017/5/23.
+//  Copyright © 2017年 fangxue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ YFXCameraTool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YFXCameraTool
+//
+//  Created by fangxue on 2017/5/23.
+//  Copyright © 2017年 fangxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
